045b671cded2deae1386c8f2711bb0bb9d72833f
inch50 <private90y@gmail.com>
2026-04-27 01:02:41 +0300

    Delete functions/hello.js

--- functions/hello.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  const name = url.searchParams.get('name') || 'World';
-  return new Response(`<h1>Hello, ${name}!</h1>`, {
-    headers: { 'Content-Type': 'text/html' },
-  });
-}
