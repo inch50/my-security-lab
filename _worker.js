f023a6d40acc5d048f1fe07064bfdf7924c7a644
inch50 <private90y@gmail.com>
2026-04-27 01:42:31 +0300

    Delete _worker.js

--- _worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request) {
-    const url = new URL(request.url);
-    const name = url.searchParams.get('name') || 'World';
-    return new Response(`<h1>Hello, ${name}!</h1>`, {
-      headers: { 'Content-Type': 'text/html' },
-    });
-  },
-};
